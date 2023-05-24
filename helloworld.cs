430ab233321f579c55034935b29f8f3121b2203b
smachni <134525104+smachni@users.noreply.github.com>
2023-05-24 21:00:11 +0600

    Add files via upload
    загрузил скрипт helloworld

---
+++ helloworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //ghgk
+    void Start()
+    {
+        print("function Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("fucntion update");
+    }
+}
